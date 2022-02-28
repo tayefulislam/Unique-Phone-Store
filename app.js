12fb048e2a74ebda6f491998ff1153a988233dae
tayeful912 <kazitayefulislam12@gmail.com>
2022-03-01 04:22:43 +0600

    create modal by click complete

--- app.js
+++ app.js
@@ -80,4 +80,33 @@ const details = (id) => {
     fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
         .then(res => res.json())
         .then(data => console.log(data))
+
+
+    const modalId = document.getElementById('detail-modal');
+    modalId.innerHTML = `
+    
+
+    <div class="modal-header">
+    <h5 class="modal-title" id="detailsModalLabel">Modal title</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="modal"
+        aria-label="Close"></button>
+    </div>
+
+    <div class="modal-body">
+            <div>
+
+            </div>
+
+    </div>
+    
+    `
+
+    console.log(modalId)
+
+
+
+
 }
+
+
+
